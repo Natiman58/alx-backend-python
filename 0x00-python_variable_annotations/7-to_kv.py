63a4fb334f1d3b5487c05bfcb29f4dd7d15f91bf
Natiman58 <natiman58@gmail.com>
2023-01-05 17:50:03 +0300

    Tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Annotated function that takes a string k and v (an int or float)
+    as argument and returns a tuple
+"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes k(string) and v(int or float)
+        and returns a tuple of str and float"""
+    return (k, v**2)
