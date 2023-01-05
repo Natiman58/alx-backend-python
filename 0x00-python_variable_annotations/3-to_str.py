3d87a39b14c47d588ae8efd2a3d263e687d3e0d2
Natiman58 <natiman58@gmail.com>
2023-01-05 13:24:27 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    A function that takes a float as an input
+    and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string represntation of the n"""
+    return str(n)
