9163b19734e3971c5bf467269f8f8b188de1e71a
Natiman58 <natiman58@gmail.com>
2023-01-05 13:00:33 +0300

    string concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    A function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ adds the 2 strings and return concatenated string"""
+    return str1 + str2
