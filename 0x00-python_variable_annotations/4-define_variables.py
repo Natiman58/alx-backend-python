9ff6c0ef732801d94d8f260e7a37b8466fc602f6
Natiman58 <natiman58@gmail.com>
2023-01-05 13:58:02 +0300

    variable annotation

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Annotation for variables
+"""
+from typing import List
+
+a: List[int] = 1
+pi: List[float] = 3.14
+i_understand_annotations: List[bool] = True
+school: List[str] = "Holberton"
