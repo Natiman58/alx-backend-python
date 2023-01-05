8f81f97a56b14140de977073318cf69da0e5b173
Natiman58 <natiman58@gmail.com>
2023-01-05 14:00:18 +0300

    no import

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,9 +2,8 @@
 """
     Annotation for variables
 """
-from typing import List
 
-a: List[int] = 1
-pi: List[float] = 3.14
-i_understand_annotations: List[bool] = True
-school: List[str] = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
