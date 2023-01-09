f9487a4a3840005681a238ca8726c9d219f4f432
Natiman58 <natiman58@gmail.com>
2023-01-09 22:29:48 +0300

    List

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,10 +3,12 @@
     return a result as a list
 """
 import asyncio
+from typing import List
+
 task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
         waits for the wait_random function to complete
         return the results as a list
