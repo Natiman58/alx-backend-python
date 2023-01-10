a97e69f38e099015b84e2787a28d7db723907478
Natiman58 <natiman58@gmail.com>
2023-01-10 13:08:00 +0300

    measure runtime after 4 executions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    A coroutine that measures a run time    
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+        excecutes async_compression fun 4 times
+        using asyncio.gather
+    """
+    start_time = time.time()
+    
+    for i in range(3):
+        await asyncio.gather(async_comprehension())
+
+    end_time = time.time()
+    
+    return (end_time - start_time)
