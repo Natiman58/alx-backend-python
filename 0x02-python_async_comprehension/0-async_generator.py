72eaa37a3e40fec56a17cc3c38cb6eab9c5cfe67
Natiman58 <natiman58@gmail.com>
2023-01-10 12:11:17 +0300

    wait for one sec then yield random number between 1 to 10

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,9 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator:
     """
-        Loops 10 times and each time the loop waits one sec
+        Loops 10 times and each time the loop waits for one sec
         then yields a random number between 0 - 10
     """
     for i in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
