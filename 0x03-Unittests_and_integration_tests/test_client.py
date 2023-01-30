1f0df259b6688195b0f060b65519bc830b367f5f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-30 12:28:09 +0300

    mock org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python3
+"""
+This is a simple script to run the main.py file.
+"""
+
+from typing import Dict
+import unittest
+from unittest.mock import MagicMock, patch
+from parameterized import parameterized
+import client
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    This is a test class for the GithubOrgClient class.
+    """
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch('client.get_json')  # patch where the method is called
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        """Tests the `org` method."""
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = client.GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
